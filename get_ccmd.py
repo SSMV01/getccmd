5d652d84df72a8da3b9bd0cb7b185576784afdca
SSMV01 <shivaswaroopmv2@gmail.com>
2023-02-27 02:12:06 +0530

    new file

--- get_ccmd.py
+++ get_ccmd.py
@@ -14,9 +14,10 @@ print('-' * 20)
 print("Starting installation")
 
 chdir(f'/home/{USERNAME}')
+
 retry = 0
+
 def install_dependencies():
-    global retry
     logging.info("Installing dependencies...")
     with Popen([sys.executable, '-m', 'pip', 'install', 'colorama'], stdout=PIPE, stderr=PIPE) as install_colorama:
         if b"No module named pip" in install_colorama.stderr.read():
@@ -27,24 +28,21 @@ def install_dependencies():
 
             logging.info("Installing pip...")
             with Popen([ sys.executable ,'-m', 'ensurepip', '--upgrade'], stdout=PIPE, stderr=PIPE) as install_pip:
-                if install_pip.stderr.read() != b'':
+                if install_pip.stderr.read() == None:
+                    logging.info("pip installed.")
+                    check_call(f'export PATH=/home/{USERNAME}/.local/bin:$PATH')
+                    retry += 1
+                    install_dependencies()
+                else:
                     logging.error("Failed to install pip")
                     logging.info("exiting...")
                     sys.exit(2)
-
-                else:
-                    logging.info("pip installed.")
-                    os.environ['PATH'] = f'/home/{USERNAME}/.local/bin:' + os.environ['PATH']
-                    retry += 1
-                    install_dependencies()
-
         elif install_colorama.stderr.read() != b'':
             print(install_colorama.stderr.read())
-
         else:
             with Popen(['echo', '$PATH'], stdout=PIPE) as verify_path:
                 if b'/home/{USERNAME}/.local/bin' not in verify_path.stdout.read():
-                    os.environ['PATH'] = f'/home/{USERNAME}/.local/bin:' + os.environ['PATH']
+                    check_call(f'export PATH=/home/{USERNAME}/.local/bin:$PATH', shell=True)
             logging.info("Done.")
 
 def clone():
@@ -53,7 +51,6 @@ def clone():
         with Popen(['git', 'clone', 'https://github.com/ssmv01/ccmd'], stdout=PIPE, stderr=PIPE) as clone_ccmd:
             if b"Could not resolve host: github.com" in clone_ccmd.stderr.read():
                 logging.error("Failed to access github.com")
-
             else:
                 logging.info("Done.")
 
@@ -93,7 +90,6 @@ def main():
             sys.exit(0)
         else:
             setup()
-
     else:
         install_dependencies()
         clone()
